7d3c3175e8ca282915dd6149d3d5e1ef2a09e508
Georgy <georgy.fesenko@revolut.com>
2020-02-19 22:29:30 +0300

    New project with R.swift and swiftlint embedded created

---
+++ TinkoffMidProject/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TinkoffMidProject
+//
+//  Created by Georgy Fesenko on 19/02/2020.
+//  Copyright © 2020 TinkoffFintech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
