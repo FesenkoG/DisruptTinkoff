ee0ecf980faade0401b0e90833e2532e17deab73
Artyom Kudryashov <ocbnishi@gmail.com>
2020-02-23 17:53:29 +0300

    Removed useless code.

--- TinkoffMidProject/ViewController.swift
+++ TinkoffMidProject/ViewController.swift
@@ -11,41 +11,5 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
-hideKeyboardOnBackgroundTap()
-        let ptf = PlainTextField(title: "titleeee")
-        ptf.delegate = self
-        view.addSubview(ptf)
-        NSLayoutConstraint.activate(
-            [
-                ptf.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
-                ptf.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
-                ptf.widthAnchor.constraint(equalToConstant: 200)
-            ]
-        )
-    }
-
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if textField.text == "1234" {
-            (textField as? PlainTextField)?.error = "errorr4g531"
-        } else {
-(textField as? PlainTextField)?.error = ""
-        }
-    }
-
-
-}
-
-
-
-extension UIViewController {
-    func hideKeyboardOnBackgroundTap() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
     }
 }
